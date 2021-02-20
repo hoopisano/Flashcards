500540552d5fbd185e96aabe04856e29bb9fdee1
Andrew Pisano <andrewpisano3@gmail.com>
2021-02-20 18:40:51 -0500

    Rounded corners

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,14 +12,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var Question: UILabel!
     @IBOutlet weak var Answer: UILabel!
     
+    @IBOutlet weak var Card: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Card.layer.cornerRadius = 20.0
+        Card.clipsToBounds = true
+        Card.layer.shadowRadius = 15.0
+        Card.layer.shadowOpacity = 0.2
     }
 
     @IBAction func didTapFlashcard(_ sender: Any) {
         Question.isHidden = true
     }
     
+    @IBAction func didTapAnswer(_ sender: Any) {
+        Question.isHidden = false
+    }
 }
 
