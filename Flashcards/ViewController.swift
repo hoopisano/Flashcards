b9bfc8a1b55aafdf0ba654b884c43dd2f7689b9f
Andrew Pisano <andrewpisano3@gmail.com>
2021-02-21 12:17:18 -0500

    working as of sunday 2/21

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,26 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var Question: UILabel!
-    @IBOutlet weak var Answer: UILabel!
-    
-    @IBOutlet weak var Card: UIView!
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Card.layer.cornerRadius = 20.0
-        Card.clipsToBounds = true
-        Card.layer.shadowRadius = 15.0
-        Card.layer.shadowOpacity = 0.2
     }
 
-    @IBAction func didTapFlashcard(_ sender: Any) {
-        Question.isHidden = true
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
     }
     
-    @IBAction func didTapAnswer(_ sender: Any) {
-        Question.isHidden = false
-    }
 }
 
